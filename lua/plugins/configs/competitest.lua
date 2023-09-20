0d5ce2ef2118a31f9ef3b9dc242f11cf8eefd460
jerrywcy <jerrymonkeynj@163.com>
2023-09-20 16:32:32 -0400

    feat: commander

--- lua/plugins/configs/competitest.lua
+++ lua/plugins/configs/competitest.lua
@@ -27,10 +27,21 @@ return {
             testcases_output_file_format = "$(TCNUM).out",
 
             template_file = "~/OI/.templates/template.$(FEXT)",
+            date_format = "%Y-%m-%d %H:%M:%S",
             received_problems_path = "$(HOME)/OI/$(JUDGE)/$(CONTEST)/$(PROBLEM).$(FEXT)",
             received_contests_directory = "$(HOME)/OI/$(JUDGE)/$(CONTEST)",
             received_contests_problems_path = "$(PROBLEM)/main.$(FEXT)",
             evaluate_template_modifiers = true,
         })
     end,
+    commander = {
+        {
+            cmd = "<CMD>CompetiTest receive problem<CR>",
+            desc = "Receive problem with CompetiTest",
+        },
+        {
+            cmd = "<CMD>CompetiTest receive contest<CR>",
+            desc = "Receive contest with CompetiTest",
+        },
+    }
 }
--- lua/plugins/configs/undotree.lua
+++ lua/plugins/configs/undotree.lua
@@ -52,96 +52,114 @@
 -- +-----------------------------------+
 
 return {
-	"mbbill/undotree",
-	config = function()
-		local g = vim.g
+    "mbbill/undotree",
+    config = function()
+        local g = vim.g
 
-		g.undotree_WindowLayout = 4
+        g.undotree_WindowLayout = 4
 
-		-- e.g. using 'd' instead of 'days' to save some space
-		g.undotree_ShortIndicators = 0
+        -- e.g. using 'd' instead of 'days' to save some space
+        g.undotree_ShortIndicators = 0
 
-		-- undotree window width
-		if g.undotree_ShortIndicators == 1 then
-			g.undotree_SplitWidth = 24
-		else
-			g.undotree_SplitWidth = 30
-		end
+        -- undotree window width
+        if g.undotree_ShortIndicators == 1 then
+            g.undotree_SplitWidth = 24
+        else
+            g.undotree_SplitWidth = 30
+        end
 
-		-- diff window height
-		g.undotree_DiffpanelHeight = 10
+        -- diff window height
+        g.undotree_DiffpanelHeight = 10
 
-		-- auto open diff window
-		g.undotree_DiffAutoOpen = 1
+        -- auto open diff window
+        g.undotree_DiffAutoOpen = 1
 
-		-- if set, let undotree window get focus after being opened,
-		-- otherwise focus will stay in current window
-		g.undotree_SetFocusWhenToggle = 1
+        -- if set, let undotree window get focus after being opened,
+        -- otherwise focus will stay in current window
+        g.undotree_SetFocusWhenToggle = 1
 
-		-- tree node shape
-		g.undotree_TreeNodeShape = "*"
+        -- tree node shape
+        g.undotree_TreeNodeShape = "*"
 
-		-- tree vertical shape
-		g.undotree_TreeVertShape = "|"
+        -- tree vertical shape
+        g.undotree_TreeVertShape = "|"
 
-		-- tree split shape
-		g.undotree_TreeSplitShape = "/"
+        -- tree split shape
+        g.undotree_TreeSplitShape = "/"
 
-		-- tree return shape
-		g.undotree_TreeReturnShape = "\\"
+        -- tree return shape
+        g.undotree_TreeReturnShape = "\\"
 
-		g.undotree_DiffCommand = "diff"
+        g.undotree_DiffCommand = "diff"
 
-		-- relative timestamp
-		g.undotree_RelativeTimestamp = 1
+        -- relative timestamp
+        g.undotree_RelativeTimestamp = 1
 
-		-- Highlight changed text
-		g.undotree_HighlightChangedText = 1
+        -- Highlight changed text
+        g.undotree_HighlightChangedText = 1
 
-		-- Highlight changed text using signs in the gutter
-		g.undotree_HighlightChangedTextWithSign = 1
+        -- Highlight changed text using signs in the gutter
+        g.undotree_HighlightChangedTextWithSign = 1
 
-		-- Highlight linked syntax type
-		-- You may choose your favorite through ":hi" command
-		g.undotree_HighlightSyntaxAdd = "DiffAdd"
-		g.undotree_HighlightSyntaxAdd = "DiffChange"
-		g.undotree_HighlightSyntaxAdd = "DiffDelete"
+        -- Highlight linked syntax type
+        -- You may choose your favorite through ":hi" command
+        g.undotree_HighlightSyntaxAdd = "DiffAdd"
+        g.undotree_HighlightSyntaxAdd = "DiffChange"
+        g.undotree_HighlightSyntaxAdd = "DiffDelete"
 
-		-- show help line
-		g.undotree_HelpLine = 1
+        -- show help line
+        g.undotree_HelpLine = 1
 
-		-- show cursorline
-		g.undotree_CursorLine = 1
+        -- show cursorline
+        g.undotree_CursorLine = 1
 
-		-- User commands
-		local usrcommand = vim.api.nvim_create_user_command
-		usrcommand("UndotreeToggle", ":call undotree#UndotreeToggle()", {
-			bar = true,
-			nargs = 0,
-			desc = "Toggle Undo Tree",
-		})
-		usrcommand("UndotreeHide", ":call undotree#UndotreeHide()", {
-			bar = true,
-			nargs = 0,
-			desc = "Hide Undo Tree",
-		})
-		usrcommand("UndotreeShow", ":call undotree#UndotreeShow()", {
-			bar = true,
-			nargs = 0,
-			desc = "Show Undo Tree",
-		})
-		usrcommand("UndotreeFocus", ":call undotree#UndotreeFocus()", {
-			bar = true,
-			nargs = 0,
-			desc = "Focus Undo Tree",
-		})
+        -- User commands
+        local usrcommand = vim.api.nvim_create_user_command
+        usrcommand("UndotreeToggle", ":call undotree#UndotreeToggle()", {
+            bar = true,
+            nargs = 0,
+            desc = "Toggle Undo Tree",
+        })
+        usrcommand("UndotreeHide", ":call undotree#UndotreeHide()", {
+            bar = true,
+            nargs = 0,
+            desc = "Hide Undo Tree",
+        })
+        usrcommand("UndotreeShow", ":call undotree#UndotreeShow()", {
+            bar = true,
+            nargs = 0,
+            desc = "Show Undo Tree",
+        })
+        usrcommand("UndotreeFocus", ":call undotree#UndotreeFocus()", {
+            bar = true,
+            nargs = 0,
+            desc = "Focus Undo Tree",
+        })
 
-		local mappings = {
-			n = {
-				["<leader>u"] = { "<cmd> UndotreeToggle <CR>", "Toggle Undo Tree" },
-			},
-		}
+        local mappings = {
+            n = {
+                ["<leader>u"] = { "<cmd> UndotreeToggle <CR>", "Toggle Undo Tree" },
+            },
+        }
 
-		require("core.utils").load_mappings(mappings)
-	end,
+        require("core.utils").load_mappings(mappings)
+    end,
+    commander = {
+        {
+            cmd = "<cmd>UndotreeToggle<CR>",
+            desc = "Toggle Undo Tree",
+        },
+        {
+            cmd = "<cmd>UndotreeShow<CR>",
+            desc = "Show Undo Tree",
+        },
+        {
+            cmd = "<cmd>UndotreeHide<CR>",
+            desc = "Hide Undo Tree",
+        },
+        {
+            cmd = "<cmd>UndotreeFocus<CR>",
+            desc = "Focus Undo Tree",
+        },
+    }
 }
