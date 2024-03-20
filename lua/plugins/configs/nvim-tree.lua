230727000107f03461933391dae66d508036ab06
jerrywcy <jerrymonkeynj@163.com>
2024-03-20 11:28:41 -0400

    fix: remove tag for nvim-tree

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
 	dependencies = {
 		"nvim-tree/nvim-web-devicons", -- optional, for file icons
 	},
-	tag = "nightly", -- optional, updated every week. (see issue #1193)
 	config = function()
 		local g = vim.g
 
