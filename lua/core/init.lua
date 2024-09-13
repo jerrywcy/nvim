0cc282cb0738d6309796ab1a458f465edac7dcd0
jerrywcy <jerrymonkeynj@163.com>
2024-09-13 14:14:36 -0400

    feat: remove autostart telescope project on start

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,15 +2,15 @@ require("core.options")
 require("core.mappings")
 
 if next(vim.fn.argv()) == nil then
-    vim.api.nvim_create_autocmd("VimEnter", { command = "NvimTreeToggle", })
-    vim.api.nvim_create_autocmd("VimEnter", { command = "Telescope project", })
+	vim.api.nvim_create_autocmd("VimEnter", { command = "NvimTreeToggle" })
+	-- vim.api.nvim_create_autocmd("VimEnter", { command = "Telescope project", })
 end
 
 local uv = vim.loop
 
 local function file_exists(path)
-    local stat = uv.fs_stat(path)
-    return stat and stat.type == 'file'
+	local stat = uv.fs_stat(path)
+	return stat and stat.type == "file"
 end
 
 -- if not file_exists('platform-specific.lua') then
